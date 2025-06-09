64ddbf7e951df746392a7946d3f11cd74feb0225
GnomoCarek <renanamoriim@gmail.com>
2025-06-09 15:42:23 -0300

    Alteração na page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 'use client';
 import Cards from "./components/CardsProdutos";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { retornarProdutos } from "./services";
 
 export default function Home() {
   const [listaProdutos, setListaProdutos] = useState(retornarProdutos());
@@ -8,6 +10,11 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <h1>DevCake</h1>
+      <section className={styles.secaoprodutos}>
+        <div className={styles.spacer}>
+          <Cards produtos={listaProdutos} />
+        </div>
+      </section>
     </div>
   );
 }
