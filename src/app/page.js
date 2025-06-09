4ca76f914f72a2f2b368dddf49eb7696dbb8720d
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2025-06-09 14:28:10 -0300

    DELETE estruturas

--- src/app/page.js
+++ src/app/page.js
@@ -4,23 +4,12 @@ import Contato  from "@/app/components/Contato/index";
 
 export default function Home() {
   return (
-    <div>
-      <header>
-        <div className={styles.page}>
-          <h1>Topo</h1>
-        </div>
-      </header>
-      <main>
-        <div className={styles.page}>
-          <h1>Main</h1>
-        </div>
-      </main>
+    <div>      
       <footer>
         <div className={styles.page}>
           <Contato/>
         </div>
-      </footer>
-       
+      </footer>       
     </div>       
   );
 }
